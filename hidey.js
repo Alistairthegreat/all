3dd3b902789269e13a56cffe1e4a16cc21705c32
Oliver Salameh <69776090+SipSup3314@users.noreply.github.com>
2023-03-21 17:19:01 -0400

    a cool tab hider update

--- hidey.js
+++ hidey.js
@@ -1,6 +1,7 @@
 if (!window.top.location.href.startsWith("https://skybase-alpha.github.io/")) {
   document.querySelector("title").innerHTML = "Google";
   function hidey() {
+    var theURL = prompt("Please enter a URL to show in your history instead of Shadow's Games\r\nMake sure to include https:// or http://\r\nLeave blank for Google Classroom");
     function inFrame() {
       try {
         return window.self !== window.top;
@@ -14,7 +15,12 @@ if (!window.top.location.href.startsWith("https://skybase-alpha.github.io/")) {
         '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;}</style><body><iframe height="100%" width="100%" src="' +
         window.location.href +
         '"frameborder="0" allowfullscreen></iframe></body></html>';
-      window.location.replace("https://classroom.google.com/h");
+      if(theURL=="" || theURL==null)
+      {
+        window.location.replace("https://classroom.google.com/h");
+      } else {
+        window.location.replace(theURL);
+      }
     }
   }
 } else {
